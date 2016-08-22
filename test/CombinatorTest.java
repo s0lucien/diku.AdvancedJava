99dfc470bdd33b16fa539a1d4b552807475b301e
martinmetaksov <info@metaksov.dk>
2016-08-22 14:01:50 +0200

    combinator test

--- test/CombinatorTest.java
+++ test/CombinatorTest.java
@@ -1,24 +1,39 @@
 import assignment1.Employee;
 import org.junit.Test;
 
-import static junit.framework.TestCase.assertEquals;
-
 public class CombinatorTest {
 
-    final int MIN_AGE = 20;
-    final int MAX_AGE = 60;
-    final int MIN_SALARY = 3000;
-    final int MAX_SALARY = 5000;
+    private final int MIN_AGE = 20;
+    private final int MAX_AGE = 60;
+    private final int MIN_SALARY = 3000;
+    private final int MAX_SALARY = 5000;
 
     @Test
-    public void testSomething() {
-        assertEquals(5, 5);
+    public void testAddSalaryForAssociativity() {
+        for (int i = 0; i < 1000; i++) {
+            Employee e_1 = genEmployee();
+            Employee e_2 = genEmployee();
+//            AddSalary
+//            assertEquals(e_1)
+        }
+    }
+
+    @Test
+    public void testAddSalaryForNeutralElement() {
+
+    }
+
+    @Test
+    public void testLongestNameForAssociativity() {
+
+    }
+
+    @Test
+    public void testLongestNameForNeutralElement() {
+
     }
 
     private Employee genEmployee() {
-//        int randomAge = ;
-//        int randomSalary = ;
-
         return new Employee("John Doe", MIN_AGE + (int)(Math.random() * MAX_AGE), MIN_SALARY + (int)(Math.random() * MAX_SALARY) );
     }
 }
