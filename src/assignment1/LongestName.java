d5f62ea75711bc2fe62db7feff749efcd590b2d3
martinmetaksov <info@metaksov.dk>
2016-08-22 13:34:40 +0200

    ex 1 b

---
+++ src/assignment1/LongestName.java
@@ -0,0 +1,22 @@
+package assignment1;
+
+public class LongestName implements Combinator<Employee, String> {
+
+    @Override
+    public String neutral() {
+        return null;
+    }
+
+    @Override
+    public String combine(String x, String y) {
+        if (x.length() == y.length()) {
+            return x.compareToIgnoreCase(y) > 0 ?  x : y; // in case of equality, it does not matter wether x or y is returned
+        } else
+            return x.length() > y.length() ? x : y;
+    }
+
+    @Override
+    public String get(Employee x) {
+        return x.getName();
+    }
+}
