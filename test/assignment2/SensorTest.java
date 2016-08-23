1eab52e837523ee0b6ebeb3695e89d6889b14394
martinmetaksov <info@metaksov.dk>
2016-08-23 21:37:54 +0200

    assignment 2 ex 1 fixed random float generator and removed senseless test

--- test/assignment2/SensorTest.java
+++ test/assignment2/SensorTest.java
@@ -2,39 +2,12 @@ package assignment2;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-
 public class SensorTest {
 
     @Test
     public void testSensorReadingGeneration() {
-        ASensor sensor = new ASensor();
-        new Thread(sensor).start();
-
-        try {
-            Thread.currentThread().sleep(3000);
-            Monitor m_1 = new AMonitor();
-            Monitor m_2 = new AMonitor();
-            ArrayList<Monitor> monitors = new ArrayList<>();
-            new Thread() {
-                public void run() {
-                    monitors.add(m_1);
-                }
-            }.start();
-
-            new Thread() {
-                public void run() {
-                    monitors.add(m_2);
-                }
-            }.start();
-
-
-            sensor.registerMonitor(monitors);
-            Thread.currentThread().sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-
+//        ASensor sensor = new ASensor();
+//        new Thread(sensor).start();
+//        todo: make this test sensible
     }
 }
